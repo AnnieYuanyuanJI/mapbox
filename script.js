3592f8080751339095dab27b6465f5535252fcbc
AnnieYuanyuanJI <55907917+AnnieYuanyuanJI@users.noreply.github.com>
2021-10-19 10:51:16 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uaWV5dWFueXVhbiIsImEiOiJja3V5Y2w5ODcwMTJnMm9xMWV4c2xzcnA3In0.a0avWlw9_6fNRL4owL6phQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
